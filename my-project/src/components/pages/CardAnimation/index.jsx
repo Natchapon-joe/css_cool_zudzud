97297faf364a849115a99917fc7306ea313bed2f
JoeyForDev <joeyfordev@N4LCHs-MacBook-Pro.local>
2023-11-06 10:12:30 +0700

    cardAnimation

---
+++ my-project/src/components/pages/CardAnimation/index.jsx
@@ -0,0 +1,10 @@
+import CardAnimation from "../../molecules/CardAnimation";
+export default function CardAnimations() {
+  return (
+    <div>
+      <div>
+        <CardAnimation />
+      </div>
+    </div>
+  );
+}
